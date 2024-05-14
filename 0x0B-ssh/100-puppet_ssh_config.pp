8769b059048554f6ab281260bdb9e04d0739c48a
Nosipho2021 <lorrainemdk@gmail.com>
2024-05-14 12:36:19 -0700

    Message describing your changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,20 @@
+# Ensure that the parent directory exists
+file { '/home/root':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+}
+
+# Define SSH directory
+file { '/home/root/.ssh':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    require => File['/home/root'],
+}
+
 # Define SSH client configuration for connecting to the server
 file { '/home/root/.ssh/config':
     ensure  => present,
