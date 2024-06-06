19ddaff760d4241fb383df2f7f42732b63110427
root <lorrainemdk@gmail.com>
2024-06-06 04:08:48 -0700

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,6 +2,7 @@
 """ Module for storing the count_words function. """
 from requests import get
 
+
 def count_words(subreddit, word_list, word_count=[], page_after=None):
     """
     Prints the count of the given words present in the title of the
