f78440448b977c74a2b1bab6ef432fe09a83ae4e
root <lorrainemdk@gmail.com>
2024-06-10 06:33:00 -0700

    update

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Logins in the user and open a file without any error message.
+
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
+}
